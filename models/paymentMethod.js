85def7b4097e425f83c0997a833daf7c9ea704af
Arthur Pasquoto Marcon <arthurmarcon17@gmail.com>
2024-10-24 09:01:09 -0300

    paymenthod.js alteracoes

--- models/paymentMethod.js
+++ models/paymentMethod.js
@@ -3,7 +3,7 @@ const pool = require('../config/db');
 const PaymentMethod = {
     insertPaymentMethod: async (name, description) => {
         const result = await pool.query(
-            'INSERT INTO payment_method (name, description) VALUES ($1, $2) RETURNING id',
+            'INSERT INTO payment_method (method, description) VALUES ($1, $2) RETURNING id_payment_method',
             [name, description]
         );
         return result.rows[0].id;
@@ -15,17 +15,17 @@ const PaymentMethod = {
     },
 
     getPaymentMethodByName: async (name) => {
-        const result = await pool.query('SELECT * FROM payment_method WHERE name = $1', [name]);
+        const result = await pool.query('SELECT * FROM payment_method WHERE method = $1', [name]);
         return result.rows[0];
     },
 
     deletePaymentMethod: async (id) => {
-        await pool.query('DELETE FROM payment_method WHERE id = $1', [id]);
+        await pool.query('DELETE FROM payment_method WHERE id_payment_method = $1', [id]);
     },
 
     editPaymentMethod: async (id, name, description) => {
         const result = await pool.query(
-            'UPDATE payment_method SET name = $1, description = $2 WHERE id = $3 RETURNING *',
+            'UPDATE payment_method SET method = $1, description = $2 WHERE id_payment_method = $3 RETURNING *',
             [name, description, id]
         );
         return result.rows[0];
